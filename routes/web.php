70645281d15b5091a53b968f9384726b9a368d6b
jim <abdullahalhasan.cse@gmail.com>
2026-03-31 20:00:18 +0400

    horizon set up

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 
 Route::get('/generate-catalogue/{id}', function ($id) {
 
-    \App\Jobs\GenerateAuctionCatalogJob::dispatch($id);
+    \App\Jobs\GenerateAuctionCatalogJob::dispatch($id)->onQueue('pdf');
 });
 
 Route::get('/testing-pdf','App\Http\Controllers\TestController@test');
